b51fc9113b65b308364f28626e531f7a0b9ccfdb
Luiz <luiz@farsoft.com.br>
2016-09-06 17:19:51 -0300

    Aula-02

---
+++ js/main.js
@@ -0,0 +1,25 @@
+/**
+ * Created with JetBrains PhpStorm.
+ * User: Luiz
+ * Date: 06/09/16
+ * Time: 17:09
+ * To change this template use File | Settings | File Templates.
+ */
+
+var list = [
+    {"desc":"rice", "amount":"1", "value" : "5.40"},
+    {"desc":"beer", "amount":"12", "value" : "1.99"},
+    {"desc":"meat", "amount":"2", "value" : "15.00"}
+];
+
+function getTotal(list){
+    var total = 0;
+
+    for (var key in list){
+        total += list[key].value * list[key].amount;
+    }
+    return total;
+
+}
+
+console.log(getTotal(list));
