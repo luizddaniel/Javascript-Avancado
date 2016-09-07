a33a74e214ca6c02c9a18c4d71dbdb805a1ad264
Luiz <luiz@farsoft.com.br>
2016-09-07 17:31:24 -0300

    Aula-07

--- js/main.js
+++ js/main.js
@@ -73,6 +73,8 @@ function setUpdate(id){
     document.getElementById("btnUpdate").style.display = "inline-block";
     document.getElementById("btnAdd").style.display = "none";
 
+    document.getElementById("inputIDUpdate").innerHTML = '<input id="idUpdate" type="hidden" value="'+id+'">';
+
 }
 
 function resetForm(){
@@ -84,7 +86,24 @@ function resetForm(){
     document.getElementById("btnUpdate").style.display = "none";
     document.getElementById("btnAdd").style.display = "inline-block";
 
+    document.getElementById("inputIDUpdate").innerHTML = "";
 
 }
+
+function updateData(){
+
+    var id = document.getElementById("idUpdate").value;
+
+    var desc = document.getElementById("desc").value;
+    var amount = document.getElementById("amount").value;
+    var value = document.getElementById("value").value;
+
+    list[id] = {"desc": desc,"amount": amount, "value": value };
+
+    resetForm();
+    setList(list);
+}
+
+
 setList(list);
 console.log(getTotal(list));
